8be844f7c8493f20df856ab4e78dd066bf3eb45a
Providence Salumu <Providence.Salumu@smunix.com>
2021-01-03 11:32:51 -0500

    nixpkgs: bump nixpkgs-unstable -> master

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   description = "dratrion configuration";
   inputs = {
-    nixpkgs.url = github:NixOs/nixpkgs/nixpkgs-unstable;
-    # nixpkgs.url = github:NixOs/nixpkgs/master;
+    # nixpkgs.url = github:NixOs/nixpkgs/nixpkgs-unstable;
+    nixpkgs.url = github:NixOs/nixpkgs/master;
     nix.url = github:NixOs/nix/master;
     nixos.url = github:NixOs/nixpkgs/nixos-unstable-small;
     nur.url = github:nix-community/NUR;
