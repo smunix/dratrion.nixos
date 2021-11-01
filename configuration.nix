cfd0098ff76a632362cfe2ed0228c05c1a916568
Providence Salumu <Providence.Salumu@smunix.com>
2021-11-01 05:43:41 -0400

    Revert "nvidia: add back nvidia"
    This reverts commit 0cf13fbe7f81906ba0209c35b22465f6a212b608.

--- configuration.nix
+++ configuration.nix
@@ -38,9 +38,6 @@
       	};
       };
     };
-    nvidia = {
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
-    };
   };
 
   # Enable sound.
@@ -545,7 +542,7 @@
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
   services.xserver = {
-    videoDrivers = [ "nvidia" "nouveau" "modsetting"  ];
+    videoDrivers = [ "nouveau" "modsetting"  ];
     windowManager = {
       awesome = {
         enable = true;
