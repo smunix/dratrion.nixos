5b4f9682460be641f229451cd20f5c1f533bdc08
Providence Salumu <Providence.Salumu@smunix.com>
2021-12-03 05:36:51 -0500

    add cache.nixos.org

--- configuration.nix
+++ configuration.nix
@@ -208,7 +208,7 @@
     trustedUsers = [ "smunix" ];
     binaryCachePublicKeys =
       [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
-    binaryCaches = [ "https://hydra.iohk.io" ];
+    binaryCaches = [ "https://hydra.iohk.io" "https://cache.nixos.org/" ];
     gc = {
       automatic = true;
       dates = "daily";
