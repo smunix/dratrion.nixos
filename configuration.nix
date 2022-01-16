b65dcc5cf207562331526c37ed76c2fe0537c262
Providence Salumu <Providence.Salumu@smunix.com>
2022-01-16 00:40:20 -0500

    gnome3: s/defaultIconTheme/adwaita-icon-theme

--- configuration.nix
+++ configuration.nix
@@ -189,12 +189,12 @@
     description = "udiskie to automount removable media";
     wantedBy = [ "default.target" ];
     path = with pkgs; [
-      gnome3.defaultIconTheme
+      gnome3.adwaita-icon-theme
       gnome3.gnome_themes_standard
       udiskie
     ];
     environment.XDG_DATA_DIRS =
-      "${pkgs.gnome3.defaultIconTheme}/share:${pkgs.gnome3.gnome_themes_standard}/share";
+      "${pkgs.gnome3.adwaita-icon-theme}/share:${pkgs.gnome3.gnome_themes_standard}/share";
     serviceConfig.Restart = "always";
     serviceConfig.RestartSec = 2;
     serviceConfig.ExecStart = "${pkgs.udiskie}/bin/udiskie -a -t -n -F ";
